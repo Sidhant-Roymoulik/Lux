4bf350180197365e9a92aa150444b5221eb39853
Sidhant Roymoulik <sidhant.roymoulik@gmail.com>
2023-09-01 20:35:03 -0400

    Added chess library and switched to C++17

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    if (__cplusplus == 202002L)
+        std::cout << "C++20\n";
+    else if (__cplusplus == 201703L)
+        std::cout << "C++17\n";
+    else if (__cplusplus == 201402L)
+        std::cout << "C++14\n";
+    else if (__cplusplus == 201103L)
+        std::cout << "C++11\n";
+    else if (__cplusplus == 199711L)
+        std::cout << "C++98\n";
+    else
+        std::cout << "pre-standard C++\n";
+}
