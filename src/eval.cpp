aa822ce23336d2703540e0687629cf9fdff01ab1
Sidhant Roymoulik <sidhant.roymoulik@gmail.com>
2024-02-03 16:07:48 -0500

    added comments to eval

--- src/eval.cpp
+++ src/eval.cpp
@@ -17,6 +17,7 @@ int eval_pawn(EvalInfo &info, const Board &board) {
     int score   = 0;
     Bitboard bb = board.pieces(PieceType::PAWN, c);
 
+    // Add pawn bb to eval info
     info.pawn[(int)c] = bb;
 
     while (bb) {
@@ -36,11 +37,15 @@ int eval_piece(EvalInfo &info, const Board &board) {
     Bitboard bb = board.pieces(p, c);
     info.gamephase += phase_values[(int)p] * builtin::popcount(bb);
 
-    const Direction UP_EAST = c == Color::BLACK ? Direction::NORTH_EAST : Direction::SOUTH_EAST;
-    const Direction UP_WEST = c == Color::BLACK ? Direction::NORTH_WEST : Direction::SOUTH_WEST;
+    // Init useful directions
+    const Direction DOWN_EAST = c == Color::BLACK ? Direction::NORTH_EAST : Direction::SOUTH_EAST;
+    const Direction DOWN_WEST = c == Color::BLACK ? Direction::NORTH_WEST : Direction::SOUTH_WEST;
 
-    Bitboard pawn_attacks = attacks::shift<UP_EAST>(info.pawn[(int)~c]) | attacks::shift<UP_WEST>(info.pawn[(int)~c]);
+    // Init enemy pawn attacks
+    Bitboard pawn_attacks =
+        attacks::shift<DOWN_EAST>(info.pawn[(int)~c]) | attacks::shift<DOWN_WEST>(info.pawn[(int)~c]);
 
+    // Bishop pair bonus
     if (p == PieceType::BISHOP && (bb & (bb - 1))) {
         score += bishop_pair;
     }
@@ -48,6 +53,7 @@ int eval_piece(EvalInfo &info, const Board &board) {
     while (bb) {
         Square sq = builtin::poplsb(bb);
 
+        // Open and Semi-Open file bonus
         if ((int)p >= 3) {
             Bitboard file = attacks::MASK_FILE[(int)utils::squareFile(sq)];
             if (!(file & info.pawn[(int)c])) {
@@ -59,10 +65,12 @@ int eval_piece(EvalInfo &info, const Board &board) {
             }
         }
 
+        // Penalty is piece is attacked by pawn
         if (pawn_attacks & (1ULL << sq)) {
             score += attacked_by_pawn[c == board.sideToMove()];
         }
 
+        // Get move bb for piece
         Bitboard moves = 0;
         if (p == PieceType::KNIGHT)
             moves = attacks::knight(sq);
@@ -73,6 +81,7 @@ int eval_piece(EvalInfo &info, const Board &board) {
         else if (p == PieceType::QUEEN || p == PieceType::KING)
             moves = attacks::queen(sq, board.occ());
 
+        // Bonus/penalty for number of moves per piece
         score += mobility[(int)p - 1][builtin::popcount(moves & ~board.us(c) & ~pawn_attacks)];
 
         if (c == Color::WHITE) sq = sq ^ 56;
@@ -102,6 +111,7 @@ void eval_pieces(EvalInfo &info, const Board &board) {
 }
 
 double endgame_scale(EvalInfo &info) {
+    // Divide the endgame score if the stronger side doesn't have many pawns left
     int num_missing_stronger_pawns = 8 - builtin::popcount(info.pawn[info.score < 0]);
     return (128 - num_missing_stronger_pawns * num_missing_stronger_pawns) / 128.0;
 }
