32aa6b1d5ecb125f8e2140ad742d8bfd3961b75e
Sidhant Roymoulik <sidhant.roymoulik@gmail.com>
2026-04-15 19:17:59 -0400

    tune: update SPSA search parameters from tune run
    Bench: 4128348

--- src/search_params.h
+++ src/search_params.h
@@ -5,31 +5,31 @@
 struct SearchParams {
     // Aspiration Windows
     int asp_window    = 12;
-    int asp_divisor   = 16777;
-    int asp_delta_div = 5;
+    int asp_divisor   = 15963;
+    int asp_delta_div = 6;
 
     // Reverse Futility Pruning
-    float rfp_margin = 64.97f;
-    int rfp_depth    = 14;
+    float rfp_margin = 59.75f;
+    int rfp_depth    = 13;
 
     // Null Move Pruning
-    float nmp_base    = 2.29f;
-    float nmp_divisor = 2.74f;
+    float nmp_base    = 2.35f;
+    float nmp_divisor = 2.77f;
 
     // Late Move Reductions
-    float lmr_base    = 2.37f;
-    float lmr_divisor = 1.92f;
+    float lmr_base    = 2.15f;
+    float lmr_divisor = 1.81f;
     int lmr_move_min  = 2;
 
     // History
-    int hist_prune       = 5201;
-    float hist_bonus_mul = 0.73f;
+    int hist_prune       = 5161;
+    float hist_bonus_mul = 0.66f;
 
     // Time Management
-    float tm_soft_multiplier = 0.60f;
-    float tm_hard_multiplier = 2.00f;
-    float tm_pv_floor        = 0.50f;
-    float tm_pv_base         = 2.00f;
+    float tm_soft_multiplier = 0.65f;
+    float tm_hard_multiplier = 2.44f;
+    float tm_pv_floor        = 0.54f;
+    float tm_pv_base         = 1.82f;
     float tm_pv_decay        = 0.10f;
 };
 
